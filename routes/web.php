2a09b2a300f72edb21451d39f667c44f729ff84c
Riza Asyifudin Faiz <djahilang133@gmail.com>
2021-04-14 01:51:50 +0700

    riza

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,6 @@ Route::resource('tools', ToolsController::class);
 
 Route::get('/alamat', [AlamatController::class, 'index']);
 Route::get('/product', [ProductController::class, 'index']);
+
+// Tambahan Route untuk memanggil controller User
+Route::post('/addtocartajax', [ChartController::class, 'ajaxaddtocart']);
